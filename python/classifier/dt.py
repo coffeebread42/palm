12eefe09b9a5697182325986013e9ac83a630fda
coffeebread42 <36703167+coffeebread42@users.noreply.github.com>
2018-12-10 22:01:39 +0000

    Add files via upload
    decision tree

---
+++ python/classifier/dt.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from scipy import stats
+
+
+### Input
+filename = "flv2.out"
+
+#gamma = 0.001
+#cost = 10000
+n_trails = 10
+test_percentage = 0.33
+
+### Process
+data = np.genfromtxt(filename, delimiter=' ')
+
+
+y = data[:, 0]
+X = data[:, 1:]
+o_a=[]
+s_s=[]
+s_p=[]
+
+for t in range(n_trails):
+# Break the data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = test_percentage, random_state = None)
+# Develop a model
+    model = DecisionTreeClassifier(max_depth=5, min_samples_split=0.3, min_samples_leaf=0.3)
+    model.fit(X_train, y_train)
+# Apply the model
+    y_hat = model.predict(X_test)
+# Overall accuray
+    oa = np.mean(y_test == y_hat)
+# Sensitivity = proportion of positive correctly classified
+    ss = np.sum(np.logical_and(y_hat == 1, y_test == 1)) / np.sum(y_test == 1)
+# Specificity = proportion of negatives classified
+    sp = np.sum(np.logical_and(y_hat == 0, y_test == 0)) / np.sum(y_test == 0)
+# Print results
+    print(t, " ", oa, " ", ss, " ", sp)
+#make list from the results of oa, ss, sp
+
+#append results
+    o_a.append(oa)
+    s_s.append(ss)
+    s_p.append(sp)
+
+
+
+
+
+#transform list into array
+oa1=np.array(o_a)
+ss1=np.array(s_s)
+sp1=np.array(s_p)
+#descriptive stats for oa
+min_oa = oa1.min()
+max_oa = oa1.max()
+mean_oa = oa1.mean()
+std_oa = oa1.std()
+
+#descriptive stats for ss
+min_ss = ss1.min()
+max_ss = ss1.max()
+mean_ss = ss1.mean()
+std_ss = ss1.std()
+
+#descriptive stats for sp
+min_sp = sp1.min()
+max_sp = sp1.max()
+mean_sp = sp1.mean()
+std_sp = sp1.std()
+
+#print values oa
+print(min_oa," ", max_oa," ",mean_oa," ",std_oa)
+
+
+#print values ss
+print(min_ss," ", max_ss," ",mean_ss," ",std_ss)
+
+#print values sp
+print(min_sp," ", max_sp," ",mean_sp," ",std_sp)
